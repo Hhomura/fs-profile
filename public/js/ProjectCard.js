ba42749faa1d513cce400b5f3556163c5de13c9d
Felipe Santos <felipesantosramos2004@gmail.com>
2023-05-09 11:28:30 -0400

    Some changes

--- public/js/ProjectCard.js
+++ public/js/ProjectCard.js
@@ -1,31 +1,29 @@
 var data = [
     {
-        Titulo: "primeiro Titulo",
-        descricao: "primeira AAAAAAAAAAAAAAAAAAAAAAA descrição do projeto",
+        Titulo: "Communication",
+        descricao: "Jogo desenvolvido como projeto de Iniciação Científica (PIBIC) durante o curso Técnico em Jogos Digitais no IFAM.",
+        desctech: "Unity | C#",
         link1: "#",
         link2: "#"
     },
     {
-        Titulo: "primeiro Titulo",
-        descricao: "primeira descrição do projeto",
+        Titulo: "Task List",
+        descricao: "Projeto Pessoal Desenvolvido com intuito de organização e gerenciamento de Tarefas Simples.",
+        desctech: "React.js | JSON.Server",
         link1: "#",
         link2: "#"
     },
     {
-        Titulo: "primeiro Titulo",
-        descricao: "primeira descrição do projeto",
-        link1: "#",
-        link2: "#"
-    },
-    {
-        Titulo: "primeiro Titulo",
-        descricao: "primeira descrição do projeto",
+        Titulo: "CRUD Postgres",
+        descricao: "Projeto Pessoal que faz ligação com o banco de dados para um sistema simiples de cadastro e relação de dados.",
+        desctech: "Node.js | PostgresSQL | Bootstrap | handlebars",
         link1: "#",
         link2: "#"
     },
     {
         Titulo: "Em breve",
         descricao: "projeto em andamento...",
+        desctech: "...",
         link1: "#",
         link2: "#"
     },
@@ -39,6 +37,7 @@ function createCards() {
         var clone = document.importNode(cardTemplate.content, true);
         var titulo = clone.querySelector("h3");
         var descricao = clone.querySelector("p");
+        var descTech = clone.querySelector("#descTech");
         var link1 = clone.querySelector("#link1");
         var link2 = clone.querySelector("#link2");
 
@@ -46,6 +45,7 @@ function createCards() {
         descricao.textContent = item.descricao;
         link1.textContent = item.link1;
         link2.textContent = item.link2;
+        descTech.textContent = item.desctech;
 
         container.appendChild(clone);
     });
